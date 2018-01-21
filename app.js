4e63f1c6b6426e428d8d5c3b8f652f514614b2c9
enduroRider <luki.bigos@gmail.com>
2018-01-21 22:10:23 +0100

    develop - quiz basic funct

---
+++ app.js
@@ -0,0 +1,44 @@
+(function() {
+    function Question (question, answers, correctAnswer) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+    
+    Question.prototype.showQuestion = function() {
+        console.log(this.question);
+    
+        for(i = 0; i < this.answers.length; i++) {
+            console.log(i + ' : ' + this.answers[i]);
+        }
+    }
+    
+    Question.prototype.checkAnswer = function(ans) {
+        if(ans === this.correctAnswer) {
+            console.log('correct!');
+        }
+        else {
+            console.log('incorrect :/');
+        }
+    }
+    
+    var q1 = new Question('What\'s your favourite city?', 
+                        ['Kraków', 'Lizbon', 'Barcelona'], 0);
+    
+    var q2 = new Question('What\'s your favourite bike?', 
+                        ['Giant Reign', 'Kross Moon','Trek Fuel'], 1);
+    
+    var q3 = new Question('Are you progressing constantly on JS?', 
+                        ['Yes', 'No'], 0);
+    
+    var questions = [q1, q2, q3];
+    
+    var randomQ = Math.floor(Math.random() * questions.length);
+    
+    questions[randomQ].showQuestion();
+    
+    var userAnswer = parseInt(prompt('Please provide the answer'));
+    
+    questions[randomQ].checkAnswer(userAnswer);
+    
+})();
